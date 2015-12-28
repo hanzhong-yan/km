4ea4396d413dc1a78fbad3ff15b191c795e3a8a6
hanzhong-yan <center.yan@gmail.com>
2015-12-29 00:01:55 +0800

    it is simple but work fine.

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 'use strict'
+
+process.env.DEBUG = 'koa*,km';
+
+var util = require("util");
+
 var koa = require("koa");
 var app = koa();
 var router = require("koa-router")();
 var views = require('koa-views');
 
+var logger = require('debug');
+var debug= require('debug')('km');
 
 //logger 
-
 app.use(function *(next){
     var start = new Date;
     yield next;
     var ms = new Date - start ; 
     console.log('%s %s - %s', this.method , this.url , ms);
-});
+}); 
 
 //Must be used before any router is used 
 app.use(views('views',{
@@ -20,45 +26,27 @@ app.use(views('views',{
         html : 'ejs'
     }
 }));
+ 
+app.use(function *(next){
+    debug('this.render : %j' , this.render);
+    debug('router: %j' , router);
+    yield next;
+});
 
 
-//response
-app.use(function *(){
-    // this.body = 'Hello World!';
-    this.state = {
-        session : this.session , 
-        title : 'app'
-    };
-    yield this.render('user',{
-        user : 'John'
+    router.get('/',function *(next){
+        debug("enter / ");
+        yield this.render('user');
+    }); 
+    router.get('/account',function *(next){
+        debug("enter /account ");
+        yield this.render('user');
+    }); 
+    router.get('/user',function *(){
+        debug("enter /user");
+        yield this.render('user',{user:{name:'hanzhong.yan'}});
     });
-}); 
 
 app.use(router.routes())
    .use(router.allowedMethods());
-
 app.listen(3000);
-
-router
-    .get('/',function *(next){
-        this.body = 'Hello World!';
-    })
-    .get('/users',function *(next){
-        // this.body = ['hanzhong.yan'];
-        this.render('user');
-    })
-    .get('/user',function *(){
-        this.render('user',{user:'harry'});
-    });
-
-
-
-/* var cfg = {
-    version : "1.1",
-    name : '',
-    createTime :'2015年 12月 19日 星期六 22:00:29 CST' 
-};
-
-console.log('fdasfdsal');
- */
-
